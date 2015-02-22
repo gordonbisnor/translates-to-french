017a8e45a70e791c83346957e9e662a57bc9f9bb
Gordon B. Isnor <gordonbisnor@gmail.com>
2015-02-22 12:48:39 -0400

    debugging

--- lib/translates_to_french.rb
+++ lib/translates_to_french.rb
@@ -21,38 +21,37 @@ module TranslatesToFrench
 
   module InstanceMethods
 
-    private
-      def column_value column
-          if self[column_name_translated(column)]
-           translated_value_if_available column
-          else
-             regular_value column
-          end
-      end    
-  
-      def translated_value_if_available column        
-         if self[column_name_translated(column)].length > 0
-            translated_value column
-          else
-            regular_value column
-          end
-      end
-  
-      def translated_value column
-         self[column_name_translated(column)]
-      end
-  
-      def regular_value column
-         self[column]
-      end
-
-      def column_name_translated(name)
-        if I18n.locale == 'fr' || I18n.locale == :fr
-          column_name = "#{name}_fr"
+    def column_value column
+        if self[column_name_translated(column)]
+         translated_value_if_available column
+        else
+           regular_value column
         end
-        self.class.column_names.include?(column_name) ? column_name.to_sym : "#{name}".to_sym
+    end    
+
+    def translated_value_if_available column        
+       if self[column_name_translated(column)].length > 0
+          translated_value column
+        else
+          regular_value column
+        end
+    end
+
+    def translated_value column
+       self[column_name_translated(column)]
+    end
+
+    def regular_value column
+       self[column]
+    end
+
+    def column_name_translated(name)
+      if I18n.locale == 'fr' || I18n.locale == :fr
+        column_name = "#{name}_fr"
       end
-       
+      self.class.column_names.include?(column_name) ? column_name.to_sym : "#{name}".to_sym
+    end
+     
   end
 
 end
